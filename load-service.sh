77387b84c4ef0c8447440d74a15fc20f7fa50d26
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-02-17 00:55:55 +0000

    🔧 Updated .gitignore file

--- load-service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Loading SystemCTL modtracker service"
-
-sudo systemctl start modtracker
-
-echo "Started modtracker service!"
-
-sudo journalctl -u modtracker
--- reload-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Reloading SystemCTL"
-
-sudo systemctl daemon-reload
-
-echo "SystemCTL was reloaded successfully!"
